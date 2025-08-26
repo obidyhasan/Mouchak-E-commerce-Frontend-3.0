98200b436bd0ada685dc9dd40c7fdd6acd809eb4
obidyhasan <obidyhasan@gmail.com>
2025-08-26 12:49:25 +0600

    fixed gtm code

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -8,20 +8,30 @@ declare global {
   }
 }
 
+/**
+ * Push custom event to GTM dataLayer
+ */
 export function track(event: string, params: Record<string, any> = {}) {
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({ event, ...params });
+  if (typeof window !== "undefined") {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({ event, ...params });
+  }
 }
 
+/**
+ * Hook: pushes virtual pageviews to GTM on every route change
+ */
 export function useGtmVirtualPageview() {
   const location = useLocation();
 
   useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: "virtual_page_view",
-      page_path: location.pathname + location.search,
-      page_title: document.title || undefined,
-    });
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "virtual_page_view",
+        page_path: location.pathname + location.search,
+        page_title: document.title || undefined,
+      });
+    }
   }, [location.pathname, location.search]);
 }
